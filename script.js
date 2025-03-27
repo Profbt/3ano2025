173deeff858a6258624c5f8c8ff4269d0c9614d6
Professor Bruno Carvalho <101364708+Profbt@users.noreply.github.com>
2025-03-27 19:30:17 -0300

    ww

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
     botaoDeAcessibilidade.addEventListener('click' , function () {
         botaoDeAcessibilidade.classlist.toggle('rotacao-botao');
         opcoesDeAcessibilidade.classlist.togg('apresenta-lista');
-    })
+    });
     
 
     // Acessa o botão com o ID 'aumentar-fonte' no HTML e o armazena em uma variÃ¡vel
